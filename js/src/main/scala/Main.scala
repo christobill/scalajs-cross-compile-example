a8a7c4a7439500edadfd073cc214063f13ef18b0
Christophe Billiard <christophe.billiard@besedo.com>
2018-01-03 11:36:37 +0100

    js/jvm are formatted but not shared

--- js/src/main/scala/Main.scala
+++ js/src/main/scala/Main.scala
@@ -2,8 +2,8 @@ import scala.scalajs.js
 
 object Main extends js.JSApp {
   def main(): Unit = {
-    val functionFatArrow: Int => Int = x => x+1
-    val lib = new MyLibrary
+    val functionFatArrow: Int ⇒ Int = x ⇒ x + 1
+    val lib                          = new MyLibrary
     println(lib.sq(2))
   }
 }
--- jvm/src/main/scala/Main.scala
+++ jvm/src/main/scala/Main.scala
@@ -1,7 +1,7 @@
 object Main {
   def main(args: Array[String]): Unit = {
-    val functionFatArrow: Int => Int = x => x+1
-    val lib = new MyLibrary
+    val functionFatArrow: Int ⇒ Int = x ⇒ x + 1
+    val lib                          = new MyLibrary
     println(lib.sq(2))
   }
 }
